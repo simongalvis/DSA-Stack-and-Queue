5ccafaabfa5e90ff94dd90f79b9a93d960b2fbac
Simon Galvis <simongalvis9@gmail.com>
2020-12-13 23:11:24 -0700

    Initialize '_Node' and 'Stack' classes

---
+++ practice.js
@@ -0,0 +1,15 @@
+class _Node{
+    constructor(data, next){
+        this.data = data;
+        this.next = next;
+    }
+}
+class Stack{
+    constructor(){
+        this.top = null;
+    }
+
+    //push
+
+    //pop
+}
